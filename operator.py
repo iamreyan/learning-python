21affc9e7bd0a47cd6147d39abac0a30d8581b2a
Reyan A <iamreyan@proton.me>
2023-12-10 11:56:43 +0500

    my first code

---
+++ operator.py
@@ -0,0 +1,14 @@
+a = '@'          #storing text in a variable
+d = '$'          #storing text in a variable
+n = 5            #storing numbers in a variable
+N = 5            #storing numbers in a variable  
+
+print(a * n)
+
+print(d * n)
+
+print(a + ' ' + d)
+
+print(a * n)
+
+print(d * n)
