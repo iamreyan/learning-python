d0e5b3e501ef904a0cfe22a7a98a4a7668a89314
Reyan A <iamreyan@proton.me>
2024-02-19 19:08:35 +0500

    my python projects

--- triangle-pattern.py
+++ triangle-pattern.py
@@ -1,17 +1,17 @@
 t = 6
 d = '$'
 space = ' '
-# for y in range(6):
-#     print(d*y)
+for y in range(6):
+    print(d*y)
 
-# for y in range(6,0,-1):
-#     print(d*y)
+for y in range(6,0,-1):
+    print(d*y)
 
 for y in range(5):
     s = t - y
     print(s*space,end='')
     print(d*y)
-    # print(s+d)
+    print(s+d)
 
 
 
